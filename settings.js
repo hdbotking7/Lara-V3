b5cb177f26a603dd1e1db662481a761de102bc35
hdbotking7 <168343086+hdbotking7@users.noreply.github.com>
2025-03-03 10:12:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "ejxCCR5A#MrdF9cH-LJroMDPr93CpXzKH7wCCRkXXTtvi97jkTtY", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
